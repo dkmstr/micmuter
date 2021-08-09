70fc1bf8d5fefe3f46b4727ac7f7adead44b0ea7
Adolfo Gómez <dkmater@dkmon.com>
2021-08-09 15:43:10 +0200

    Initial

---
+++ src/muter/__init__.py
@@ -0,0 +1,2 @@
+from . import keycodes
+from . import keys
---
+++ src/muter/keys.py
@@ -0,0 +1,13 @@
+import typing
+
+# Windows libs
+import win32api
+
+__all__ = ['isKeyPressed']
+
+# Get if key is pressed
+def isKeyPressed(key: int) -> bool:
+    return win32api.GetAsyncKeyState(key) & 0x8000
+
+
+
